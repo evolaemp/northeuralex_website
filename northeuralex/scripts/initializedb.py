c61b055c1227df1476148623567a0b6c523023d2
pavelsof <pavelsof@gmail.com>
2017-06-07 16:54:04 +0200

    The initializedb script can now read datasets.

--- northeuralex/scripts/initializedb.py
+++ northeuralex/scripts/initializedb.py
@@ -1,21 +1,99 @@
+import collections
+import csv
+
 from clld.db.meta import DBSession
 from clld.db.models import common
 from clld.scripts.util import initializedb, Data
 
 
 
-def main(args):
-    """
-    Populates the database. Expects: (1) the db to be empty; (2) main_data and
-    lang_data to be present in the args argparse.Namespace instance.
-    
-    This function is called within a db transaction, the latter being handled
-    by initializedb.
-    """
-    data = Data()
+class LangDataset:
+	"""
+	Handles reading the NorthEuraLex' language data dataset.
+	"""
 
-    dataset = common.Dataset(id='northeuralex', domain='northeuralex.clld.org')
-    DBSession.add(dataset)
+	class LangDatasetDialect(csv.Dialect):
+		"""
+		Describes the tsv dialect used for the language data file.
+		"""
+		delimiter = '\t'
+		lineterminator = '\r\n'
+		quoting = csv.QUOTE_NONE
+		strict = True
+
+
+	Language = collections.namedtuple('Language', [
+		'iso_code', 'glotto_code', 'latitude', 'longitude', 'name'])
+
+
+	def __init__(self, dataset_fp):
+		"""
+		Constructor.
+		"""
+		self.dataset_fp = dataset_fp
+
+
+	def gen_langs(self):
+		"""
+		Yields a Language named tuple at a time.
+		"""
+		with open(self.dataset_fp, 'r', encoding='utf-8') as f:
+			reader = csv.reader(f, dialect=self.LangDatasetDialect)
+			for row in reader:
+				yield self.Language._make(row)
+
+
+
+class MainDataset:
+	"""
+	Handles reading the main NorthEuraLex dataset.
+	"""
+
+	class MainDatasetDialect(csv.Dialect):
+		"""
+		Describes the tsv dialect used for the dataset file.
+		"""
+		delimiter = '\t'
+		lineterminator = '\r\n'
+		quoting = csv.QUOTE_NONE
+		strict = True
+
+
+	Word = collections.namedtuple('Word', [
+		'iso_code', 'glotto_code', 'concept', 'form', 'ipa'])
+
+
+	def __init__(self, dataset_fp):
+		"""
+		Constructor.
+		"""
+		self.dataset_fp = dataset_fp
+
+
+	def gen_words(self):
+		"""
+		Yields a Word named tuple at a time.
+		"""
+		with open(self.dataset_fp, 'r', encoding='utf-8') as f:
+			reader = csv.DictReader(f, dialect=self.MainDatasetDialect)
+			for line in reader:
+				yield self.Word(line['Language_ID'], line['Glottocode'],
+						line['Concept_ID'], line['Word_Form'], line['rawIPA'])
+
+
+
+def main(cls, args):
+	"""
+	Populates the database. Expects: (1) the db to be empty; (2) main_data and
+	lang_data to be present in the args argparse.Namespace instance.
+
+	This function is called within a db transaction, the latter being handled
+	by initializedb.
+	"""
+	data = Data()
+
+	dataset = common.Dataset(id='northeuralex', domain='northeuralex.clld.org')
+	DBSession.add(dataset)
 
 
 
---
+++ northeuralex/tests/test_initializedb.py
@@ -0,0 +1,50 @@
+import os.path
+import unittest
+
+from northeuralex.scripts.initializedb import LangDataset, MainDataset
+
+
+
+FIXTURES_DIR = 'northeuralex/tests/fixtures'
+
+
+
+class LangDatasetTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset = LangDataset(os.path.join(FIXTURES_DIR, 'lang_data.tsv'))
+
+    def test_gen_langs(self):
+        langs = []
+        for lang in self.dataset.gen_langs():
+            self.assertTrue(type(lang) is LangDataset.Language)
+            langs.append(lang)
+
+        self.assertEqual(len(langs), 509)
+
+        self.assertEqual(langs[0], LangDataset.Language._make([
+            'aaa', 'ghot1243', '7.11551', '5.95663', 'Ghotuo']))
+        self.assertEqual(langs[7], LangDataset.Language._make([
+            'aah', 'abua1245', '', '', 'Abu\' Arapesh']))
+        self.assertEqual(langs[-1], LangDataset.Language._make([
+            'azz', 'high1278', '20.1554', '-97.5556', 'Highland Puebla Nahuatl']))
+
+
+
+class MainDatasetTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset = MainDataset(os.path.join(FIXTURES_DIR, 'main_data.tsv'))
+
+    def test_gen_words(self):
+        words = []
+        for word in self.dataset.gen_words():
+            self.assertTrue(type(word) is MainDataset.Word)
+            self.assertEqual(word.iso_code, 'gle')
+            self.assertEqual(word.glotto_code, 'iris1253')
+            words.append(word)
+
+        self.assertEqual(len(words), 1278)
+
+        self.assertEqual(words[0], MainDataset.Word._make([
+            'gle', 'iris1253', 'Auge::N', 'súil', 'sˠuːlʲ']))
