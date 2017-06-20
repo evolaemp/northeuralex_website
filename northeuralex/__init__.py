bef3997a60fbf58c3163577e01b18022cd2048f5
pavelsof <pavelsof@gmail.com>
2017-06-20 13:37:53 +0200

    Language family map markers.

--- northeuralex/__init__.py
+++ northeuralex/__init__.py
@@ -1,7 +1,16 @@
 from pyramid.config import Configurator
 
-# we must make sure custom models are known at database initialization!
+from clld.db.meta import DBSession
+from clld.interfaces import IMapMarker
+from clld.web.icon import ORDERED_ICONS
+
+
+"""
+Even if not used, these models should still be imported. The original comment:
+we must make sure custom models are known at database initialization!
+"""
 from northeuralex import models
+
 
 
 """
@@ -17,13 +26,53 @@ _('Parameters')
 
 
 
+"""
+Dictionary mapping language families to clld.web.icon.Icon instances. Inited in
+the main function and used in the get_map_marker hook.
+"""
+FAMILY_ICONS = {}
+
+
+
+def get_map_marker(item, req):
+    """
+    Hook called for each marker on each map. Determines the map marker for the
+    given item (the latter would be an instance of a different class depending
+    on the map). Returns the URL of the selected map marker.
+
+    In other words, makes sure that each marker on a map would consistently use
+    the same icon depending on the language family.
+
+    The idea how to achieve different markers for different language families
+    was stolen from the __init__ module of the sails clld project.
+    """
+    if isinstance(item, models.Doculect):
+        family = item.family
+    elif isinstance(item, models.Synset):
+        family = item.language.family
+    else:
+        return ''
+
+    return FAMILY_ICONS[family].url(req)
+
+
+
 def main(global_config, **settings):
     """
-    This function returns a Pyramid WSGI application.
+    Returns a Pyramid WSGI application. Apart from the clld boilerplate, it
+    orders the home sub-navigation, inits the FAMILY_ICONS dict and registers
+    the get_map_marker hook.
     """
     config = Configurator(settings=settings)
     config.include('clld.web.app')
 
     config.registry.settings['home_comp'] = ['help', 'download', 'legal', 'contact']
+
+    family_query = DBSession.query(models.Doculect.family).distinct()
+    family_query = map(lambda x: x[0], family_query)
+    for family in family_query:
+        FAMILY_ICONS[family] = ORDERED_ICONS[len(FAMILY_ICONS)]
+
+    config.registry.registerUtility(get_map_marker, IMapMarker)
 
     return config.make_wsgi_app()
